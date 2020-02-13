6327ea84d5ce402b248666570b8b5b70dea91b57
liushuli <644796024@qq.com>
2020-02-13 19:06:56 +0800

    element更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/img/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WorkOrder msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WorkOrder from '@/components/WorkOrder.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    WorkOrder
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,18 @@ const path = require('path')
 const resolve = dir => path.join(__dirname,dir)
 
 
-const BASE_URL = process.env.NODE_ENV === 'procution' ? '/' : '/'
+// const BASE_URL = process.env.NODE_ENV === 'procution' ? '/' : '/'
 
 module.exports = {
   lintOnSave: false,
-  baseUrl:BASE_URL,
+  // baseUrl:BASE_URL,
   chainWebpack: config =>{
     config.resolve.alias
     .set('@',resolve('src'))
     .set('_c',resolve('src/components'))
   },
-  procutionSourceMap: false,
-  devServer: {
-     proxy: ""
-  }
+  // procutionSourceMap: false,
+  // devServer: {
+  //    proxy: ""
+  // }
 }
